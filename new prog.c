c267a3b3a54d0581b3ea19a3e1cfb99323346efb
Kumar Santhosh <uxkumaar@gmail.com>
2019-10-10 18:32:30 +0530

    Create new prog.c
    new file commit

---
+++ new prog.c
@@ -0,0 +1,24 @@
+//
+//  sizeoffn.c
+//  c_examples
+//
+//  Created by SANTHOSH KUMAR on 06/10/19.
+//  Copyright © 2019 Kumar Santhosh. All rights reserved.
+//
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stddef.h>
+
+
+    main()
+{
+    char c;
+    short s;
+    printf("Size of char is:  %d byte(s)\n", sizeof(c));
+    printf("Size of short is:  %d byte(s)\n", sizeof(s));
+    printf("Size of float is:  %d byte(s)\n", sizeof(float));
+    ///its gud to go
+    ///3rd commit
+}
