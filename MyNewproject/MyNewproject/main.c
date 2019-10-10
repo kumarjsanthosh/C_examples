2e8af656d354ea25142c07a8169838fd0cbe4803
Kumar Santhosh <uxkumaar@gmail.com>
2019-10-10 18:44:15 +0530

    new project

---
+++ MyNewproject/MyNewproject/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MyNewproject
+//
+//  Created by SANTHOSH KUMAR on 10/10/19.
+//  Copyright © 2019 Kumar Santhosh. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
